286dc4493aeb02707cf2069b634da77b2de98654
Eli Sandberg Hellström <elisan14@elevad.umea.se>
2025-05-24 13:42:24 +0200

    cropsupport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,18 @@
 	let selectType = "none";
     //paste the text under this line
 
-            
+    
+
+
     
     //paste the text above this line   
     function updateCellSize(){
         let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
         let currentCellsize = cellSize;
+        let main = document.getElementById("main");
+        main.style.width = String(windowWidth) + "px";
+        main.style.height = String(windowHeight) + "px";
         if (windowWidth < 400){
             cellSize = 4;
         }
@@ -52,6 +58,25 @@
             itemsInGrid = itemsInGrid
         }
 }   
+    function cropImage() {
+        setTimeout(() => {
+        for (let i = 0; i < itemsInGrid.length; i++){
+            let item = itemsInGrid[i];
+            console.log(item);
+            if (item.type == "image"){
+                let div = document.getElementById(String(-item.id))
+                let img = document.getElementById(String(item.id))
+                div.style.width = String((item.cropValues.baseWidth)* cellSize) + "px";
+                div.style.height = String((item.cropValues.baseHeight) * cellSize) + "px";        
+                div.style.top = String((-item.start.y + item.cropValues.cropStart.y)*cellSize) + "px";
+                div.style.left = String((-item.start.x + item.cropValues.cropStart.x)*cellSize) + "px";
+                img.style.top = String((item.start.y - item.cropValues.cropStart.y)*cellSize) + "px";
+                img.style.left = String((item.start.x - item.cropValues.cropStart.x)*cellSize) + "px";
+            }
+        }
+
+    }, 0);
+    }
 	onMount(() => {
         window.addEventListener("resize", (event) => {
             if (gridElement) {
@@ -59,23 +84,25 @@
                 gridStart = { x: rect.left, y: rect.top }; // Store the grid's top-left corner coordinates
             }
             updateCellSize();
+            cropImage();
         });
-        if (gridElement) {//copilot suggested this
+        if (gridElement) {
             const rect = gridElement.getBoundingClientRect();
             gridStart = { x: rect.left, y: rect.top }; // Store the grid's top-left corner coordinates
             gridElement = gridElement
         }
         updateCellSize();
-	});
+        cropImage();
+    });
 </script>
-<main style="background-color: {mainBackground}; background-image: {mainBackgroundImage};">
+<main style="background-color: {mainBackground}; background-image: {mainBackgroundImage};" id = "main">
 	<article class = "grid" style="width: {gridSize.x * cellSize}px; height: {gridSize.y * cellSize}px;" bind:this={gridElement}>
 	{#each itemsInGrid as item}
             <div class = "item" class:selected = {item == selectedItem} style="width:{item.width * cellSize}px; height:{item.height*cellSize}px; top: {(item.start.y*cellSize)+20}px; left: {(item.start.x *cellSize) + gridStart.x}px; background-color: {item.backgroundColor};">
                 <input type="button" style= "top: {-5}px; left: {- 5}px; width:{item.width *cellSize+5}px; height:{item.height*cellSize+5}px;" class = "border" class:invisible = {item != selectedItem}/>
                 {#if item.type == "text"}
                 <p style="font-size: {item.fontSize}; font-family: {item.font};  color:{item.color};" 
-                    class = "textInput" id = {String(item.id)}> {item.name} </p>
+                    class = "textInput"> {item.name} </p>
                 <div style="height: 100%; width: 100%; color: transparent; background-color: transparent; font-family: {item.font}" class = "textInput"> {item.name} </div>  <!--a div that changes font size with help of text fit and then applies that font size to the text area because of lack of support of text area-->    
                 {/if}
                 {#if item.type == "image"}
